83503c2408dcd0a4bedbb8197a1cf5e2cdde5afb
Trygve Horn <trygvehorn@gmail.com>
2023-02-16 19:25:22 +0100

    call works

--- src/App.js
+++ src/App.js
@@ -3,21 +3,30 @@ import { client } from "./client";
 
 function App() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
   const getData = async () => {
     try {
       const response = await client.getEntries({
         content_type: "recipeEntry",
       });
-      console.log(response);
+      // console.log(response);
+      setData(response)
     } catch (error) {
       console.error(error);
     }
+    setLoading(false)
   };
   useEffect(() => {
     getData();
   }, []);
-  console.log(data.items[0].fields.image.fields.file.url);
-  return;
+  console.log(data);
+  return(
+    <div>
+    {loading ? <p>Loading..</p> : data ? <p>{data.items[2].fields.recipeName}</p>: <p>Error</p>}
+  </div>
+  )
 }
 
 export default App;
--- src/client.js
+++ src/client.js
@@ -1,8 +1,10 @@
 import { createClient } from "contentful";
 
 export const client = createClient({
-  accessToken: "l6CcojMUjw3N7mKEP0ef8lgCGN_ESO76FUyA4ZJqT78",
-  space: "75958qusqnzq",
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+  space: process.env.REACT_APP_SPACE_ID,
+  // accessToken: "l6CcojMUjw3N7mKEP0ef8lgCGN_ESO76FUyA4ZJqT78",
+  // space: "75958qusqnzq",
 });
 // const client = require("contentful").createClient({
 //   space: "<my_space_id>",
